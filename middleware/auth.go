3521a0cf4932d591041a0610b96558b97ce3c8cf
Brendan Ragan <lordmortis@gmail.com>
2019-04-25 15:18:47 +0800

    fix auth - wasn't generating a secret before!

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,14 +1,18 @@
 package middleware
 
 import (
+	cryptoRand "crypto/rand"
+	"crypto/subtle"
 	"database/sql"
 	"encoding/base64"
+	"encoding/binary"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
-	"github.com/pkg/errors"
 	"github.com/gofrs/uuid"
+	"github.com/pkg/errors"
 	"math"
+	"math/rand"
 	"net/http"
 	"strings"
 	"time"
@@ -25,6 +29,7 @@ type Session struct {
 type redisSession struct {
 	Model string
 	ModelID []byte
+	Secret []byte
 }
 
 var (
@@ -33,6 +38,14 @@ var (
 
 func init() {
 	expiryDuration = time.Minute
+	reader := cryptoRand.Reader
+	seedBytes := []byte{0,0,0,0,0,0,0,0}
+	num, err := reader.Read(seedBytes)
+	if num < 8 || err != nil {
+		panic("Unable to generate randomized seed!")
+	}
+	seed := binary.BigEndian.Uint64(seedBytes)
+	rand.Seed(int64(seed))
 }
 
 func Auth() gin.HandlerFunc {
@@ -41,11 +54,31 @@ func Auth() gin.HandlerFunc {
 		redisClient := ctx.MustGet("redisConnection").(*redis.Client)
 		redisPrefix := ctx.MustGet("redisPrefix").(string)
 
-		sessionID, err := ctx.Cookie("sessionID")
+		header := ctx.GetHeader("Authorization")
+		if len(header) == 0 {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "not authorized"})
+			return
+		}
+
+		headerParts := strings.Split(header, " ")
+		if len(headerParts) != 2 && headerParts[0] != "Bearer" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "not authorized"})
+			return
+		}
+
+		secretString, err := ctx.Cookie("sessionSecret")
 		if err != nil {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "not authorized"})
 			return
 		}
+
+		secret, err := base64.StdEncoding.DecodeString(secretString)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "not authorized"})
+			return
+		}
+
+		sessionID := headerParts[1]
 
 		data, err := redisClient.Get(redisPrefix + "session:" + sessionID).Result()
 		if err != nil {
@@ -60,11 +93,16 @@ func Auth() gin.HandlerFunc {
 			return
 		}
 
+		if subtle.ConstantTimeCompare(redisSession.Secret, secret) != 1 {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "not authorized"})
+			return
+		}
+
 		redisClient.Set(redisPrefix + "session:" + sessionID, data, expiryDuration)
 
 		maxAge := int(math.Round(expiryDuration.Seconds()))
 		domain := strings.Split(ctx.Request.Host, ":")[0]
-		ctx.SetCookie("sessionID", sessionID, maxAge, "/", domain, false, true)
+		ctx.SetCookie("sessionSecret", secretString, maxAge, "/", domain, false, true)
 
 		ctx.Set("AuthModelType", redisSession.Model)
 		ctx.Set("AuthModelID", redisSession.ModelID)
@@ -79,8 +117,16 @@ func AuthCreateSession(ctx *gin.Context, model string, modelID []byte) (*Session
 
 	sessionID, _ := uuid.NewV4()
 	base64ID := base64.StdEncoding.EncodeToString(sessionID.Bytes())
+	secret := []byte{0,0,0,0,0,0,0,0}
+	readLength, err := rand.Read(secret)
+	if readLength != len(secret) {
+		return nil, errors.New("unable to generate session secret")
+	} else if err != nil {
+		return nil, err
+	}
 
-	redisSession := redisSession{model, modelID}
+	base64Secret := base64.StdEncoding.EncodeToString(secret)
+	redisSession := redisSession{model, modelID, secret}
 	encodedRedisSession, err := json.Marshal(redisSession)
 	if err != nil {
 		return nil, errors.Wrap(err, "Could not serialize session")
@@ -89,7 +135,7 @@ func AuthCreateSession(ctx *gin.Context, model string, modelID []byte) (*Session
 	redisClient.Set(redisPrefix + "session:" + base64ID, encodedRedisSession, expiryDuration)
 	maxAge := int(math.Round(expiryDuration.Seconds()))
 	domain := strings.Split(ctx.Request.Host, ":")[0]
-	ctx.SetCookie("sessionID", base64ID, maxAge, "/", domain, false, true)
+	ctx.SetCookie("sessionSecret", base64Secret, maxAge, "/", domain, false, true)
 
 	session := Session{sessionID, base64ID, time.Now().Add(expiryDuration),  model, modelID}
 	return &session, nil
